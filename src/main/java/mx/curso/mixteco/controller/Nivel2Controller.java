e8e4ff656c60136c912102b4aa8453400fef17a5
ServerSepia <neto1998.mtz@hotmail.com>
2021-12-03 21:38:27 -0600

    Oh Shit!

---
+++ src/main/java/mx/curso/mixteco/controller/Nivel2Controller.java
@@ -0,0 +1,32 @@
+package mx.curso.mixteco.controller;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+import mx.curso.mixteco.entity.Usuario;
+import mx.curso.mixteco.model.Animal;
+
+
+
+@Controller
+@Slf4j
+public class Nivel2Controller {
+	
+	@PostMapping ("Nivel 2")
+	public String nivel1 () {
+		return "";
+	}
+	
+
+}
