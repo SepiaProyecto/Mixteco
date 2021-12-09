5dac37fdca7e0b6ba522f4f5789439db25498d0b
ServerSepia <neto1998.mtz@hotmail.com>
2021-12-08 19:18:13 -0600

    actual

--- src/main/java/mx/curso/mixteco/controller/Nivel1Controller.java
+++ src/main/java/mx/curso/mixteco/controller/Nivel1Controller.java
@@ -102,6 +102,78 @@ RestTemplate restTemplate = new RestTemplate();
 		return "nivel1/numeronivel1";
 	}
 	
+	
+	//Codigoparaanimal
+	@PostMapping("/animalnivel1")
+	public String animalnivel1(Model model) {
+	Usuario usuario= new Usuario();
+    model.addAttribute("usuario",usuario);
+	log.info("--------nivel1 numero---------2");
+	  Usuario user=	nn.nombreglobal();
+	
+	
+RestTemplate restTemplate = new RestTemplate();
+    
+	String fooResourceUrlcontador= "http://localhost:1337/numeros/count?nivel=nivel1";
+	
+	ResponseEntity<String> contador
+	  = restTemplate.getForEntity(fooResourceUrlcontador + "", String.class);
+
+	log.info("contador xd "+contador.getBody());
+	int numero=Integer.parseInt(contador.getBody());
+	
+    List<Numeros> numeros_list=new ArrayList<>();
+   
+    List<String> list_respuestas=new ArrayList<>();
+    List<Respuesta> list_uno=new ArrayList<>();
+	for (int i=1 ; i<=numero; i++) {
+		Numeros numerores= new Numeros();
+		Respuesta respuesta= new Respuesta();
+	
+	String fooResourceUrl = "http://localhost:1337/numeros/"+i;
+	ResponseEntity<Numeros> response= restTemplate.getForEntity(fooResourceUrl + "", Numeros.class);
+	numerores.setNombreespanol(response.getBody().getNombreespanol());
+	numerores.setNombremixteco(response.getBody().getNombremixteco());
+	numerores.setValor1(response.getBody().getValor1());
+	numerores.setValor2(response.getBody().getValor2());
+	numerores.setValor3(response.getBody().getValor3());
+	numerores.setTextok(response.getBody().getTextok());
+	numerores.setTextok(response.getBody().getTextok());
+	numerores.setActivad(response.getBody().getActivad());
+	
+	numerores.setPregunta1(response.getBody().getPregunta1());
+	numerores.setPregunta2(response.getBody().getPregunta2());
+	numerores.setPregunta3(response.getBody().getPregunta3());
+	  
+	      
+	        int skaicius = (int) (Math.random() * 3 + 1);
+	      System.out.print("-----------------"+skaicius);
+	    
+    
+    
+  
+
+    System.out.println("XD numero "+response.getBody().getNombreespanol());
+ //   String valor="";
+    
+  
+ ///list_respuestas.add(valor);  
+//	
+	
+ 
+	numerores.setRespuestas(list_respuestas);
+	
+	numerores.setUrlcorto("http://localhost:1337"+response.getBody().getImagen().get(0).getUrl());
+	numerores.setAudiocorto("http://localhost:1337"+response.getBody().getAudio().get(0).getUrl());
+	numeros_list.add(numerores);
+	  list_uno.add(respuesta);
+	}  
+	model.addAttribute("list_resp", list_uno);
+	model.addAttribute("numeros", numeros_list);
+	
+		return "nivel1/animalnivel1";
+	}
+	
 
 
 	public List<String> aleatorio(boolean[] impresas,String[] ordenadas) {
