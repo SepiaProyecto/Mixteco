2ff9d8184ad6b01134104dfc13ec1bc99839b63b
ServerSepia <neto1998.mtz@hotmail.com>
2022-02-04 04:09:48 -0600

    actualizadoo

--- src/main/java/mx/curso/mixteco/controller/Nivel2Controller.java
+++ src/main/java/mx/curso/mixteco/controller/Nivel2Controller.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 import lombok.extern.slf4j.Slf4j;
 import mx.curso.mixteco.entity.Usuario;
+import mx.curso.mixteco.model.CreandoOraciones;
 import mx.curso.mixteco.model.Pronombres;
+import mx.curso.mixteco.model.PronombresInterrogativos;
 import mx.curso.mixteco.model.VerbosFuturoTYEN;
 import mx.curso.mixteco.model.VerbosFuturoUE;
 import mx.curso.mixteco.model.VerbosPasadoTYEN;
@@ -389,6 +391,111 @@ public class Nivel2Controller {
 					model.addAttribute("verbosfuturoue", numeros_list);
 
 					return "nivel2/verbosfuturoue";
+				}
+				
+				// -------------------------------------
+				
+				// -------------------------------------
+				@PostMapping("/creandoraciones")
+				public String creandoraciones(Model model) {
+					Usuario usuario = new Usuario();
+					model.addAttribute("usuario", usuario);
+					log.info("--------nivel2 creando oraciones--------");
+					Usuario user = nn.nombreglobal();
+
+					RestTemplate restTemplate = new RestTemplate();
+
+					String fooResourceUrlcontador = host + "/creand-oraciones/count?nivel=nivel2";
+
+					ResponseEntity<String> contador = restTemplate.getForEntity(fooResourceUrlcontador + "", String.class);
+
+				
+					int numero = Integer.parseInt(contador.getBody());
+
+					List<CreandoOraciones> numeros_list = new ArrayList<>();
+
+					List<String> list_respuestas = new ArrayList<>();
+
+					for (int i = 1; i <= numero; i++) {
+						CreandoOraciones numerores = new CreandoOraciones();
+						
+
+						String fooResourceUrl = host + "/creand-oraciones/" + i;
+						ResponseEntity<CreandoOraciones> response = restTemplate.getForEntity(fooResourceUrl + "", CreandoOraciones.class);
+						numerores.setNombre(response.getBody().getNombre());
+						numerores.setValor1(response.getBody().getValor1());
+						numerores.setValor2(response.getBody().getValor2());
+						numerores.setValor3(response.getBody().getValor3());
+
+						numerores.setActivad(response.getBody().getActivad());
+
+						numerores.setPregunta1(response.getBody().getPregunta1());
+						numerores.setPregunta2(response.getBody().getPregunta2());
+						numerores.setPregunta3(response.getBody().getPregunta3());
+
+						numerores.setRespuestas(list_respuestas);
+
+						numerores.setUrlcorto(host + response.getBody().getImagen().get(0).getUrl());
+						numerores.setAudiocorto(host + response.getBody().getAudio().get(0).getUrl());
+						numeros_list.add(numerores);
+
+					}
+
+					model.addAttribute("creandoraciones", numeros_list);
+
+					return "nivel2/creandoraciones";
+				}
+				
+				// -------------------------------------
+				
+				@PostMapping("/pronombresinte")
+				public String pronombresinte(Model model) {
+					Usuario usuario = new Usuario();
+					model.addAttribute("usuario", usuario);
+					log.info("--------nivel2 pronombres interrogativos--------");
+					Usuario user = nn.nombreglobal();
+
+					RestTemplate restTemplate = new RestTemplate();
+
+					String fooResourceUrlcontador = host + "/pronombres-interrogativos/count?nivel=nivel2";
+
+					ResponseEntity<String> contador = restTemplate.getForEntity(fooResourceUrlcontador + "", String.class);
+
+				
+					int numero = Integer.parseInt(contador.getBody());
+
+					List<PronombresInterrogativos> numeros_list = new ArrayList<>();
+
+					List<String> list_respuestas = new ArrayList<>();
+
+					for (int i = 1; i <= numero; i++) {
+						PronombresInterrogativos numerores = new PronombresInterrogativos();
+						
+
+						String fooResourceUrl = host + "/pronombres-interrogativos/" + i;
+						ResponseEntity<PronombresInterrogativos> response = restTemplate.getForEntity(fooResourceUrl + "", PronombresInterrogativos.class);
+						numerores.setNombre(response.getBody().getNombre());
+						numerores.setValor1(response.getBody().getValor1());
+						numerores.setValor2(response.getBody().getValor2());
+						numerores.setValor3(response.getBody().getValor3());
+
+						numerores.setActivad(response.getBody().getActivad());
+
+						numerores.setPregunta1(response.getBody().getPregunta1());
+						numerores.setPregunta2(response.getBody().getPregunta2());
+						numerores.setPregunta3(response.getBody().getPregunta3());
+
+						numerores.setRespuestas(list_respuestas);
+
+						numerores.setUrlcorto(host + response.getBody().getImagen().get(0).getUrl());
+						numerores.setAudiocorto(host + response.getBody().getAudio().get(0).getUrl());
+						numeros_list.add(numerores);
+
+					}
+
+					model.addAttribute("pronombresinte", numeros_list);
+
+					return "nivel2/pronombresinte";
 				}
 				
 				// -------------------------------------
