648b4d90a3d6f2c904a4a85d5589a6788565ba93
ServerSepia <neto1998.mtz@hotmail.com>
2022-06-27 13:53:35 -0500

    sdaasa

--- src/main/java/mx/curso/mixteco/controller/UserController.java
+++ src/main/java/mx/curso/mixteco/controller/UserController.java
@@ -46,7 +46,7 @@ public class UserController {
 	public String eliminar = "DELETE FROM usuariosepias " + "WHERE id=?;";
 	
 	public String SELECT_ALL = "select id, nombre, apellidopaterno, apellidomaterno, sexo, edad, usuario, contrasena FROM usuariosepias ";
-	public String editar = " UPDATE  usuariosepias  SET nombre=?, apellidopaterno=?, apellidomaterno=?, sexo=?, edad=?, usuario=? "
+	public String editar = " UPDATE  usuariosepias  SET nombre=?, apellidopaterno=?, apellidomaterno=?, sexo=?, edad=?, usuario=?, contrasena=?  "
 			+ " WHERE id= ? ";
 	
 	public String seleccionar = "SELECT id, nombre, apellidopaterno, apellidomaterno, sexo, edad, usuario, contrasena "
